f896dae2d62383f322d4b9808ddee31be1d97d9c
foulde <72756151+foulde@users.noreply.github.com>
2020-11-16 19:23:37 +0000

    Add files via upload

---
+++ Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Project_team8
+{
+    class Login
+    {
+        public static SortedList<User.Username, User> UserList;
+    }
+}
