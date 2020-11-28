7d4742042af6706b2a2facfbf1674ce9af483959
23180 <71397669+23180@users.noreply.github.com>
2020-11-28 16:24:19 +0000

    Add files via upload

--- User.cs
+++ User.cs
@@ -4,22 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace POO_Project_team8
+namespace POO_Project
 {
     class User
     {
-
         public string Username { get; set; }
-        public string Password { get; set; }
+        public string UserPassword { get; set; }
         public string UserType { get; set; }
 
-        public bool Login(List<User> UserList, string username, string password, string usertype)
+        public SortedList<string, User> FacultyUsers;
+        public SortedList<string, User> Administrators;
+        public SortedList<string, User> Students;
+        public SortedList<string, User> Users;
+        public bool Login1(SortedList<string, User> Students, SortedList<string, User> Administrators, SortedList<string, User> FacultyUsers, string username, string password, string usertype)
         {
             bool login = false;
-            //if (username)
+            if(usertype=="Faculty")
+            {
+                if (FacultyUsers.ContainsKey(username) != true) { }
 
+                else if(password==FacultyUsers[FacultyUsers.GetIndexOfKey(username)].UserPassword)
+                {   login = true;   }
+            }
 
+            else if (usertype == "Administrator")
+            {
+                if (Administrators.ContainsKey(username) != true) { }
+
+                else if (password == Administrators[Administrators.GetIndexOfKey(username)].UserPassword)
+                { login = true; }
+            }
+
+            else if (usertype == "Student")
+            {
+                if (Students.ContainsKey(username) != true) { }
+
+                else if (password == Students[Students.GetIndexOfKey(username)].UserPassword)
+                { login = true; }
+            }
+
+            return login;
         }
+
+        public bool Login2(SortedList<string, User> Users, string username, string password)
+        {
+            bool login = false;
+            if (Users.ContainsKey(username) != true) { }
+
+            else if (password == Students[Users.GetIndexOfKey(username)].UserPassword)
+            { login = true; }
+
+            return login;
+        }
+
 
     }
 }
